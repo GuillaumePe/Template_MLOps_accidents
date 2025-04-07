90c4a2410ccfc7b7e3f5359e4b6281c129c974c2
Ubuntu <ubuntu@ip-172-31-34-235.eu-west-1.compute.internal>
2025-04-07 14:35:52 +0000

    Trained and evaluated a histgradinetboosting with max_iter=200 et max_leaf_nodes=5)

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,12 +14,12 @@ y_test = pd.read_csv('data/preprocessed/y_test.csv')
 y_train = np.ravel(y_train)
 y_test = np.ravel(y_test)
 
-rf_classifier = ensemble.HistGradientBoostingClassifier()
+rf_classifier = ensemble.HistGradientBoostingClassifier(max_iter=200, max_leaf_nodes=5)
 
 #--Train the model
 rf_classifier.fit(X_train, y_train)
 
 #--Save the trained model to a file
-model_filename = 'accidents/Template_MLOps_accidents/models/trained_model.joblib'
+model_filename = './models/trained_model.joblib'
 joblib.dump(rf_classifier, model_filename)
 print("Model trained and saved successfully.")
